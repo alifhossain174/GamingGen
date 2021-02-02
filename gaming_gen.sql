b287c2ecd9d0ac5c1572259bc54dfa72e8dd275c
Fahim <alifhossain174@gmail.com>
2021-02-03 02:42:18 +0600

    Some New Features And Bug Fixing

--- gaming_gen.sql
+++ gaming_gen.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 02, 2021 at 09:14 PM
+-- Generation Time: Feb 02, 2021 at 09:41 PM
 -- Server version: 10.4.16-MariaDB
 -- PHP Version: 7.4.12
 
